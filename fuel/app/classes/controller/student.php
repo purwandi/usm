9fc584dd2a19934c81dd96f33c06b1dae85a086b
Purwandi <free6300@gmail.com>
2012-04-21 16:39:30 +0700

    Modify student controller

--- fuel/app/classes/controller/student.php
+++ fuel/app/classes/controller/student.php
@@ -2,34 +2,35 @@
 
 class Controller_Student extends Controller_Template
 {
+	public $template = 'template/layouts/default';
 
 	public function action_index()
 	{
-		$this->template->title = 'Student &raquo; Index';
+		$this->template->title = 'Student / Index';
 		$this->template->content = View::forge('student/index');
 	}
 
 	public function action_create()
 	{
-		$this->template->title = 'Student &raquo; Create';
+		$this->template->title = 'Student / Create';
 		$this->template->content = View::forge('student/create');
 	}
 
 	public function action_update()
 	{
-		$this->template->title = 'Student &raquo; Update';
+		$this->template->title = 'Student / Update';
 		$this->template->content = View::forge('student/update');
 	}
 
 	public function action_view()
 	{
-		$this->template->title = 'Student &raquo; View';
+		$this->template->title = 'Student / View';
 		$this->template->content = View::forge('student/view');
 	}
 
 	public function action_delete()
 	{
-		$this->template->title = 'Student &raquo; Delete';
+		$this->template->title = 'Student / Delete';
 		$this->template->content = View::forge('student/delete');
 	}
 
