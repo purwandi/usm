258b2fc2e1598727ff865f43df2ce52594bb9555
Purwandi <free6300@gmail.com>
2012-04-21 16:46:52 +0700

    Rename groups to group

--- fuel/app/classes/controller/group.php
+++ fuel/app/classes/controller/group.php
@@ -3,33 +3,35 @@
 class Controller_Group extends Controller_Template
 {
 
+	public $template = 'template/layouts/default';
+
 	public function action_index()
 	{
-		$this->template->title = 'Groups &raquo; Index';
+		$this->template->title = 'Groups / Index';
 		$this->template->content = View::forge('groups/index');
 	}
 
 	public function action_create()
 	{
-		$this->template->title = 'Groups &raquo; Create';
+		$this->template->title = 'Groups / Create';
 		$this->template->content = View::forge('groups/create');
 	}
 
 	public function action_update()
 	{
-		$this->template->title = 'Groups &raquo; Update';
+		$this->template->title = 'Groups / Update';
 		$this->template->content = View::forge('groups/update');
 	}
 
 	public function action_view()
 	{
-		$this->template->title = 'Groups &raquo; View';
+		$this->template->title = 'Groups / View';
 		$this->template->content = View::forge('groups/view');
 	}
 
 	public function action_delete()
 	{
-		$this->template->title = 'Groups &raquo; Delete';
+		$this->template->title = 'Groups / Delete';
 		$this->template->content = View::forge('groups/delete');
 	}
 
