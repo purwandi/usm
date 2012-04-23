b1c74deb7fa20f4136548a6ff80d1e7ffcdac6d9
Purwandi <free6300@gmail.com>
2012-04-23 14:50:32 +0700

    Add extends form and validation class

---
+++ fuel/app/classes/form.php
@@ -0,0 +1,48 @@
+<?php
+
+class Form extends Fuel\Core\Form {
+
+	public static function text ($label, $name, $value = null, array $att = array())
+	{
+		$val = Input::post($name) ? Input::post($name) : $value;
+
+		if ($error = static::error($name))
+		{
+			return '<div class="control-group error">
+				<label class="control-label error" for="form_'.$name.'">'.$label.'</label>
+				<div class="controls error">
+					 '.parent::input($name, $val, $att).'
+					 <span class="help-inline">'.$error.'</span>
+				</div>
+			</div>';
+		}
+		else
+		{
+			return '<div class="control-group">
+				<label class="control-label" for="form_'.$name.'">'.$label.'</label>
+				<div class="controls">
+					 '.parent::input($name, $val, $att).'
+				</div>
+			</div>';
+		}
+		
+	}
+
+	public static function error ($name = null)
+	{
+		$val = Session::get_flash('error');
+
+		if ($val)
+		{
+			if (isset($val[$name]))
+			{
+				return $val[$name];
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+}
