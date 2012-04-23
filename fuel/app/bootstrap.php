2743f3c8ecba76e90911f960e1b5ef3d75aabdff
Purwandi <free6300@gmail.com>
2012-04-23 14:50:50 +0700

    Add form and validation class on bootstrap

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -11,6 +11,8 @@ require COREPATH.'bootstrap.php';
 Autoloader::add_classes(array(
 	// Add classes you want to override here
 	// Example: 'View' => APPPATH.'classes/view.php',
+	'Form'			=> APPPATH.'classes/form.php',
+	'Validation'	=> APPPATH.'classes/validation.php',
 ));
 
 // Register the autoloader
