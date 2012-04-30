e5e2b4d51e6d37baa3dd60623fde58271db45112
Purwandi <free6300@gmail.com>
2012-04-30 21:39:46 +0700

    Modify admin to administrator

--- fuel/app/views/template/partials/header.php
+++ fuel/app/views/template/partials/header.php
@@ -15,7 +15,7 @@
           <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#">Member <b class="caret"></b></a>
             <ul class="dropdown-menu">
-              <li><?php echo Html::anchor('member/index/admin','Admin');?></li>
+              <li><?php echo Html::anchor('member/index/administrator','Administrator');?></li>
               <li><?php echo Html::anchor('member/index/tata-usaha','Tata Usaha');?></li>
               <li><?php echo Html::anchor('member/index/dosen','Dosen');?></li>
               <li><?php echo Html::anchor('member/index/mahasiswa','Mahasiswa');?></li>
