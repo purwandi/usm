819aecbb90847bd519c98b46cff6e69e086724be
Purwandi <free6300@gmail.com>
2012-05-03 10:36:56 +0700

    Add method pass di form class

--- fuel/app/classes/form.php
+++ fuel/app/classes/form.php
@@ -28,6 +28,32 @@ class Form extends Fuel\Core\Form {
 		
 	}
 
+	public static function pass ($label, $name, $value = null, array $att = array())
+	{
+		$val = Input::post($name) ? Input::post($name) : $value;
+
+		if ($error = static::error($name))
+		{
+			return '<div class="control-group error">
+				<label class="control-label error" for="form_'.$name.'">'.$label.'</label>
+				<div class="controls error">
+					 '.parent::password($name, $val, $att).'
+					 <span class="help-inline">'.$error.'</span>
+				</div>
+			</div>';
+		}
+		else
+		{
+			return '<div class="control-group">
+				<label class="control-label" for="form_'.$name.'">'.$label.'</label>
+				<div class="controls">
+					 '.parent::password($name, $val, $att).'
+				</div>
+			</div>';
+		}
+		
+	}
+
 	public static function error ($name = null)
 	{
 		$val = Session::get_flash('error');
