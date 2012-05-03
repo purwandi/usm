db0805de1a56ebe2e3de3a34b82e03851bc47d57
Purwandi <free6300@gmail.com>
2012-05-03 19:28:17 +0700

    Add method drop down on form class

--- fuel/app/classes/form.php
+++ fuel/app/classes/form.php
@@ -54,6 +54,33 @@ class Form extends Fuel\Core\Form {
 		
 	}
 
+	// public static function select($field, $values = null, array $options = array(), array $attributes = array())
+	public static function drop ($label, $name, $value = null, array $options = array() , array $att = array())
+	{
+		$val = Input::post($name) ? Input::post($name) : $value;
+
+		if ($error = static::error($name))
+		{
+			return '<div class="control-group error">
+				<label class="control-label error" for="form_'.$name.'">'.$label.'</label>
+				<div class="controls error">
+					 '.parent::select($name, $val, $options, $att).'
+					 <span class="help-inline">'.$error.'</span>
+				</div>
+			</div>';
+		}
+		else
+		{
+			return '<div class="control-group">
+				<label class="control-label" for="form_'.$name.'">'.$label.'</label>
+				<div class="controls">
+					 '.parent::select($name, $val, $options, $att).'
+				</div>
+			</div>';
+		}
+		
+	}
+
 	public static function error ($name = null)
 	{
 		$val = Session::get_flash('error');
