4baa8fe705ce0bae139f122c9fc20ac918467781
Purwandi <free6300@gmail.com>
2012-05-03 19:37:12 +0700

    Fix issur number #2
    Create module member tata usaha CRUD

---
+++ fuel/app/views/member/create/tata-usaha.php
@@ -0,0 +1,31 @@
+<?php echo render('member/nav-tabs');?>
+<h1>Tata Usaha</h1>
+<hr />
+<?php if (Session::get_flash('wrong')): ?>
+	<div class="alert">
+		<button class="close" data-dismiss="alert">&times;</button>
+		<?php echo Session::get_flash('wrong'); ?>
+	</div>
+<?php endif; ?>
+
+<?php echo Form::open(array('class'=>''));?>
+	<div class="row">
+		<div class="span6">
+			<?php echo Form::text('Username','username',@$member->username);?>
+			<?php echo Form::text('Email','email',@$member->email);?>
+			<?php echo Form::pass('Password','password');?>
+			<?php echo Form::pass('Confirm password','confirm_password');?>
+		</div>
+		<div class="span6">
+			<?php echo Form::text('First name','first_name',@$member->user_metadata->first_name);?>
+			<?php echo Form::text('Last name','last_name',@$member->user_metadata->last_name);?>
+			<?php echo Form::drop('Gender','gender',@$member->user_metadata->gender,array(''=>'Select gender','Laki-Laki'=>'Laki-Laki','Perempuan'=>'Perempuan'));?>
+			<?php echo Form::text('Dob','dob',@$member->user_metadata->dob);?>
+			<?php echo Form::text('Palce dob','place_dob',@$member->user_metadata->place_dob);?>
+		</div>
+	</div>
+	<div class="form-actions">
+	    <button type="submit" class="btn btn-primary">Save changes</button>
+	    <button class="btn">Cancel</button>
+	</div>
+</form>
---
+++ fuel/app/views/member/index/tata-usaha.php
@@ -0,0 +1,38 @@
+<?php echo render('member/nav-tabs');?>
+<h1>Tata Usaha</h1>
+
+<?php if (Session::get_flash('success')): ?>
+	<div class="alert alert-success">
+		<button class="close" data-dismiss="alert">&times;</button>
+		<?php echo Session::get_flash('success'); ?>
+	</div>
+<?php endif; ?>
+
+<?php if ($member):?>
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>#</th>
+			<th>Username</th>
+			<th>First Name</th>
+			<th>Last Name</th>
+			<th>Update</th>
+			<th>Delete</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach($member as $m):?>
+		<tr>
+			<td><?php echo $m->id;?></td>
+			<td><?php echo $m->username;?></td>
+			<td><?php echo $m->user_metadata->first_name;?></td>
+			<td><?php echo $m->user_metadata->last_name;?></td>
+			<td><?php echo Html::anchor($module.'/update/'.Uri::segment(3).'/'.$m->id,'Update');?></td>
+			<td><?php echo Html::anchor($module.'/delete/'.Uri::segment(3).'/'.$m->id,'Delete');?></td>
+		</tr>
+		<?php endforeach;?>
+	</tbody>
+</table>
+
+<?php endif;?>
