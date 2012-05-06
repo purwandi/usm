7af7e089051d9e688310d3d5d9ef8afe48e013f1
Purwandi <free6300@gmail.com>
2012-05-06 09:24:25 +0700

    Add form checkbox method

--- fuel/app/classes/form.php
+++ fuel/app/classes/form.php
@@ -54,7 +54,6 @@ class Form extends Fuel\Core\Form {
 		
 	}
 
-	// public static function select($field, $values = null, array $options = array(), array $attributes = array())
 	public static function drop ($label, $name, $value = null, array $options = array() , array $att = array())
 	{
 		$val = Input::post($name) ? Input::post($name) : $value;
@@ -75,6 +74,44 @@ class Form extends Fuel\Core\Form {
 				<label class="control-label" for="form_'.$name.'">'.$label.'</label>
 				<div class="controls">
 					 '.parent::select($name, $val, $options, $att).'
+				</div>
+			</div>';
+		}
+		
+	}
+
+	public static function check ($label, $name, $value = null, array $options = array() , array $att = array())
+	{
+		$val = Input::post($name) ? Input::post($name) : $value;
+
+		$data = '';
+
+		if (is_array($options) && isset($options))
+		{
+			foreach ($options as $key => $val) 
+			{
+				$att['id'] = $name.'_'.$key;
+				$data .= '<label class="checkbox">'.parent :: checkbox($name, $value, $att).$val.'</label>'."\n";
+			}
+		}
+
+		if ($error = static::error($name))
+		{
+			return '<div class="control-group error">
+				<label class="control-label error" for="form_'.$name.'">'.$label.'</label>
+				<div class="controls error">
+					 '.$data.'
+					 <span class="help-inline">'.$error.'</span>
+				</div>
+			</div>';
+		}
+		else
+		{
+			//checkbox($field, $value = null, array $attributes = array())
+			return '<div class="control-group">
+				<label class="control-label" for="form_'.$name.'">'.$label.'</label>
+				<div class="controls">
+					'.$data.'
 				</div>
 			</div>';
 		}
